4fa4c8460e823b9670470f85c225b5f518b4b89c
Yan0324 <2982764154@qq.com>
2026-03-08 16:51:28 +0800

    修改网站图标

--- web_page/src/App.vue
+++ web_page/src/App.vue
@@ -13,6 +13,7 @@
 import NavBar from './components/NavBar.vue'
 import ParticleCanvas from './components/ParticleCanvas.vue'
 import HeroSection from './components/HeroSection.vue'
+import logoUrl from './assets/logo.png'
 
 export default {
   name: 'App',
@@ -29,11 +30,36 @@ export default {
   },
   mounted() {
     document.body.setAttribute('data-theme', this.theme)
+    this.setCircularFavicon(logoUrl)
   },
   methods: {
     toggleTheme() {
       this.theme = this.theme === 'dark' ? 'light' : 'dark'
       localStorage.setItem('theme', this.theme)
+    },
+    setCircularFavicon(src) {
+      const size = 64
+      const canvas = document.createElement('canvas')
+      canvas.width = size
+      canvas.height = size
+      const ctx = canvas.getContext('2d')
+      const img = new Image()
+      img.onload = () => {
+        ctx.beginPath()
+        ctx.arc(size / 2, size / 2, size / 2, 0, Math.PI * 2)
+        ctx.closePath()
+        ctx.clip()
+        ctx.drawImage(img, 0, 0, size, size)
+        let link = document.querySelector("link[rel='icon']")
+        if (!link) {
+          link = document.createElement('link')
+          link.rel = 'icon'
+          document.head.appendChild(link)
+        }
+        link.type = 'image/png'
+        link.href = canvas.toDataURL('image/png')
+      }
+      img.src = src
     }
   }
 }
